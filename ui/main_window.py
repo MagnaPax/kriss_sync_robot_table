323867f68af289e2092f7d7230b9155e305c2cea
ChunHee Han <chunhee.han@outlook.com>
2025-10-22 13:09:31 +0900

    fix: 메서드 이름 충돌 방지
    - `python.analysis.typeCheckingMode` 세팅을 standard로 바꿔서 오류 생김

--- ui/main_window.py
+++ ui/main_window.py
@@ -7,7 +7,7 @@ MainWindow 클래스
 - 버전: 0.1.0 (2025.10.17)
 """
 
-from PyQt6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QLabel, QStatusBar, QMenuBar, QMenu
+from PyQt6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QLabel, QStatusBar
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
 from pathlib import Path
@@ -22,24 +22,23 @@ class MainWindow(QMainWindow):
         self.setGeometry(100, 100, 1200, 800)  # 초기 창 크기
 
         # 상태바 설정
-        self.statusBar = QStatusBar()
-        self.setStatusBar(self.statusBar)
+        self.status_bar = QStatusBar()
+        self.setStatusBar(self.status_bar)
         self.status_label = QLabel("Disconnected")  # 초기 상태
         self.status_label.setObjectName("status_label")  # CSS Selector 용
         self.status_label.setProperty("status", "disconnected")  # 상태 속성 설정
-        self.statusBar.addWidget(self.status_label)
+        self.status_bar.addWidget(self.status_label)
 
         # 중심 위젯과 레이아웃
-        self.centralWidget = QWidget()
-        self.setCentralWidget(self.centralWidget)
-        self.mainLayout = QHBoxLayout(self.centralWidget)
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.mainLayout = QHBoxLayout(self.central_widget)
 
 
-        # 패널
-        # 부모를 centralWidget로 명시
-        self.mainLayout.addWidget(left_panel.LeftPanel(self.centralWidget))
-        self.mainLayout.addWidget(center_panel.CenterPanel(self.centralWidget))
-        self.mainLayout.addWidget(right_panel.RightPanel(self.centralWidget))
+        # 패널 (부모를 central_widget 으로 명시)
+        self.mainLayout.addWidget(left_panel.LeftPanel(self.central_widget))
+        self.mainLayout.addWidget(center_panel.CenterPanel(self.central_widget))
+        self.mainLayout.addWidget(right_panel.RightPanel(self.central_widget))
 
         # 패널 레이아웃 비율 설정
         self.mainLayout.setStretch(0, 1)  # Left    첫째가 남는 공간 중 1만큼
