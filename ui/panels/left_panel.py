687c516ea4ad2ce94a3d04377766323bca13a48b
ChunHee Han <chunhee.han@outlook.com>
2025-10-23 11:01:07 +0900

    feat: Put 로고 위젯 into LP

--- ui/panels/left_panel.py
+++ ui/panels/left_panel.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QFrame, QVBoxLayout, QGroupBox
+from ..wigets import LogoWidget
 
 
 class LeftPanel(QFrame):
@@ -12,7 +13,7 @@ class LeftPanel(QFrame):
         layout = QVBoxLayout(self)
 
         # 위젯들
-        ci_panel = QFrame()
+        self.logo_widget = LogoWidget()
         turntable_group = QGroupBox("Turntable")
         currnet_state = QGroupBox("Current State")
         world_position = QGroupBox("World Position")
@@ -22,7 +23,7 @@ class LeftPanel(QFrame):
         world_position.setStyleSheet("color: black; border: 1px solid green;")
 
         # 위젯 추가(레이아웃에 맞춰져 추가됨)
-        layout.addWidget(ci_panel,          stretch=1)  # 1/5 -> 20%
+        layout.addWidget(self.logo_widget,  stretch=1)  # 1/5 -> 20%
         layout.addWidget(turntable_group,   stretch=2)  # 2/5 -> 40%
         layout.addWidget(currnet_state,     stretch=1)  # 1/5 -> 20%
         layout.addWidget(world_position,    stretch=1)  # 1/5 -> 20%
--- ui/wigets/__init__.py
+++ ui/wigets/__init__.py
@@ -1,3 +1,4 @@
 from .base_widget import BaseWidget
+from .logo_widget import LogoWidget
 
-__all__ = ['BaseWidget']
+__all__ = ['BaseWidget', 'LogoWidget']
