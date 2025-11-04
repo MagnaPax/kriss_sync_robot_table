31b9ef44ccdda4f1649d73c710a5ffb95d080ecf
ChunHee Han <chunhee.han@outlook.com>
2025-11-04 17:58:58 +0900

    refactor(util): Create env 파일
    - 배포된건지 아닌지 판단하는 함수 생성

---
+++ utils/env.py
@@ -0,0 +1,26 @@
+# utils/env.py
+import sys
+
+
+
+def is_packaged() -> bool:
+    if getattr(sys, 'frozen', False):
+        """
+        getattr(object, name, default) 함수:
+            sys 모듈에서 'frozen'이라는 꼬리표를 찾은 뒤 없으면 AttributeError 에러 대신 그냥 False
+
+        frozen 속성:
+            PyInstaller나 cx_Freeze 같은 프로그램으로 파이썬 스크립트를 하나의 실행 파일(.exe)로 "얼릴" 때, 
+            이 프로그램들이 sys 모듈에 frozen = True 라는 꼬리표를 붙인다
+            그러므로 sys.frozen이라는 꼬리표가 있으면 얼려진 상태(실행 파일)를 나타낸다
+
+        [시나리오]
+        1. 배포 환경:
+            True 반환 - if 실행
+
+        2. 개발 환경:
+            False 반환 - else 실행
+        """
+        return True
+    else:
+        return False
