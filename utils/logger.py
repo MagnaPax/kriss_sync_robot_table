8d63119ded4d3d479dbde867db3908d7cc59263b
ChunHee Han <chunhee.han@outlook.com>
2025-11-05 11:14:01 +0900

    refactor(util): 실행 환경 판단에 env 파일 적용

--- utils/logger.py
+++ utils/logger.py
@@ -7,42 +7,19 @@ utils/logger.py
     from utils.logger import logger
 """
 import logging
-import os, sys
 from logging.handlers import TimedRotatingFileHandler
-from pathlib import Path
+
+from .env import get_environment_base_path
 
 
+# env.py의 함수를 사용하여 기본 경로를 가져온 후 'logs' 폴더를 지정
+LOG_DIR = get_environment_base_path() / "logs"
 
-APP_NAME = "KRISSRobotPolisherSync"
-APP_AUTHOR = "Dexterous_Technology"
 
-
-if getattr(sys, 'frozen', False):
-    """
-    getattr(object, name, default) 함수:
-        sys 모듈에서 'frozen'이라는 꼬리표를 찾은 뒤 없으면 AttributeError 에러 대신 그냥 False
-
-    frozen 속성:
-        PyInstaller나 cx_Freeze 같은 프로그램으로 파이썬 스크립트를 하나의 실행 파일(.exe)로 "얼릴" 때, 
-        이 프로그램들이 sys 모듈에 frozen = True 라는 꼬리표를 붙인다
-        그러므로 sys.frozen이라는 꼬리표가 있으면 얼려진 상태(실행 파일)를 나타낸다
-
-    [시나리오]
-    1. 배포 환경:
-        True 반환 - if 실행
-
-    2. 개발 환경:
-        False 반환 - else 실행
-    """
-
-    # 배포 환경: PyInstaller, cx_Freeze 등으로 패키징된 경우
-    from platformdirs import user_data_dir
-    LOG_DIR = Path(user_data_dir(APP_NAME, APP_AUTHOR))     #  배포된 OS 표준 사용자 데이터 폴더
-else:
-    # 개발 환경
-    LOG_DIR = Path(__file__).parent.parent / "logs"         # 이 파일의 조상 폴더 밑에 logs 폴더
-
-# 폴더 생성
-# parents=True: 필요한 모든 상위 디렉터리 생성
-# exist_ok=True: 디렉터리가 이미 있어도 오케이
-LOG_DIR.mkdir(parents=True, exist_ok=True)
+# 로그 파일 저장 디렉토리 생성
+try:
+    # parents=True: 필요한 모든 상위 디렉터리 생성
+    # exist_ok=True: 디렉터리가 이미 있어도 에러 X
+    LOG_DIR.mkdir(parents=True, exist_ok=True)
+except OSError as e:
+    print(f"❌ 로그 디렉터리 생성 실패: {LOG_DIR} - {e}")
