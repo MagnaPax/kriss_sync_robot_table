2196c9a1b6b1c4785c277917d6ffe5f7a531ca60
ChunHee Han <chunhee.han@outlook.com>
2025-11-07 15:54:08 +0900

    refactor(target-position): 기본 레이아웃 재설정

--- ui/widgets/target_position_widget.py
+++ ui/widgets/target_position_widget.py
@@ -1,6 +1,6 @@
 # ui/widgets/target_position_widget.py
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QVBoxLayout, QGroupBox, QLabel
+from PyQt6.QtWidgets import QVBoxLayout, QGroupBox, QLabel, QFrame, QHBoxLayout
 
 from ui.widgets.base_widget import BaseWidget
 
@@ -15,18 +15,20 @@ class TargetPositionWidget(BaseWidget):
     def _init_ui(self):
         self.setObjectName("target_position_widget")
 
-        # 메인 레이아웃(세로 정렬)을 TargetPositionWidget(self)에 설정
-        # 이 레이아웃이 전체 위젯의 크기를 관리한다
-        main_layout = QVBoxLayout(self)
-        main_layout.setContentsMargins(0, 0, 0, 0)
-        self.setLayout(main_layout)
+        ##########################
+        # --- 기본 형태 설정 --- #
+        ##########################
 
         # 모든 부속 위젯을 담을 그룹박스 생성
-        group_box = QGroupBox("Target Position")
-        group_box.setStyleSheet("QGroupBox { background-color: #E0E0E0; }") # 개발용 임시 배경 스타일
-        main_layout.addWidget(group_box) # 메인 레이아웃이 그룹박스를 채우도록 추가
+        base_group_box = QGroupBox("Target Position")
+        base_group_box.setStyleSheet("QGroupBox { background-color: #E0E0E0; }") # 개발용 임시 배경 스타일
 
-
+        # 메인 레이아웃(세로 정렬)
+        # 이 레이아웃이 전체 위젯의 크기를 관리한다
+        main_layout = QVBoxLayout(self)  # 부모위젯(TargetPositionWidget)에 QVBoxLayout을 붙인다
+        main_layout.addWidget(base_group_box)
+        main_layout.setContentsMargins(0, 0, 0, 0)      # 레이아웃 내부 여백 제거
+        # self.setLayout(main_layout) # 부모위젯(TargetPositionWidget)에 main_layout 레이아웃 설치
 
 
 
