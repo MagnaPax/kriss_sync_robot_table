21c3745ecb26c0ac47b3f4f31fd1f33f23633f9d
ChunHee Han <chunhee.han@outlook.com>
2025-11-28 13:56:34 +0900

    refactor(path): 경로 정보 개선

--- config/paths.py
+++ config/paths.py
@@ -1,9 +1,13 @@
 # config/paths.py
-"""
-경로 정보
-"""
+"""경로 정보"""
 from pathlib import Path
 
 
-STYLESHEET_PATH = Path("styles/stylesheet.qss")
-CONFIG_MACRO_PATH = Path("config/macro_settings.json")
+
+# 프로젝트 루트 (현재 파일의 부모의 부모) -> config/paths.py -> config -> root
+ROOT_DIR = Path(__file__).resolve().parent.parent
+
+STYLESHEET_PATH = ROOT_DIR / "styles" / "stylesheet.qss"
+CONFIG_MACRO_PATH = ROOT_DIR / "config" / "macro_settings.json"
+CONFIG_INI_PATH = ROOT_DIR / "config" / "settings.ini"
+LOG_DIR = ROOT_DIR / "logs"
