be2da4bc1cb4d93b71f0d564ade57823df245bcf
ChunHee Han <chunhee.han@outlook.com>
2025-11-28 17:08:28 +0900

    feat(paths): dll 경로 추가

--- config/paths.py
+++ config/paths.py
@@ -7,7 +7,19 @@ from pathlib import Path
 # 프로젝트 루트 (현재 파일의 부모의 부모) -> config/paths.py -> config -> root
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
-STYLESHEET_PATH = ROOT_DIR / "styles" / "stylesheet.qss"
-CONFIG_MACRO_PATH = ROOT_DIR / "config" / "macro_settings.json"
+
+# 기본 설정값(앱, TwinCAT, Robot, Turtable 등)
 CONFIG_INI_PATH = ROOT_DIR / "config" / "settings.ini"
+
+# DLL 파일 경로 (libs 폴더)
+TC_ADS_DLL_PATH = ROOT_DIR / "libs" / "TcAdsDll.dll"
+
+# 스타일시트
+STYLESHEET_PATH = ROOT_DIR / "styles" / "stylesheet.qss"
+
+# 사용자 입력 매크로값 저장
+CONFIG_MACRO_PATH = ROOT_DIR / "config" / "macro_settings.json"
+
+# 로그파일 저장 경로
 LOG_DIR = ROOT_DIR / "logs"
+
