0b3aa5fc6d8764d15a4b5d65c1970d623a2132f6
ChunHee Han <chunhee.han@outlook.com>
2025-12-02 13:50:00 +0900

    chore(main-window): 스타일시트 적용 하드코딩 삭제

--- ui/main_window.py
+++ ui/main_window.py
@@ -13,8 +13,6 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
 from pathlib import Path
 from .panels import left_panel, center_panel, right_panel
-from styles.theme import load_and_apply_stylesheet
-
 
 
 class MainWindow(QMainWindow):
@@ -49,23 +47,3 @@ class MainWindow(QMainWindow):
         self.mainLayout.setStretch(0, 1)  # Left    첫째가 남는 공간 중 1만큼
         self.mainLayout.setStretch(1, 1)  # Center  둘째가 남는 공간 중 1만큼
         self.mainLayout.setStretch(2, 2)  # Right   셋째가 남는 공간 중 2만큼
-
-        # 메인 윈도우에 스타일 적용
-        self.apply_style()
-
-
-    # 스타일시트 읽기 및 적용
-    def apply_style(self):
-        """QSS 파일 읽기 및 적용"""
-        qss_path = Path("styles/stylesheet.qss")
-        
-        if qss_path.exists():
-            try:
-                with open(qss_path, "r", encoding='UTF-8') as file:
-                    stylesheet = file.read()
-                    self.setStyleSheet(stylesheet)  # MainWindow에 적용
-                    print("✅ 스타일시트 로드 성공")
-            except Exception as e:
-                print(f"❌ 스타일시트 로드 실패: {e}")
-        else:
-            print(f"⚠️ 스타일시트 파일 없음: {qss_path}")
