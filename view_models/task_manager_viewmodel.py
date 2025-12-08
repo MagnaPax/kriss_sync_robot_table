63d9f94a164463909d34319b79a455a0e78ab9e6
ChunHee Han <chunhee.han@outlook.com>
2025-12-08 14:29:16 +0900

    feat: TaskManagerWidget 의 LOAD 버튼(파일읽기) 기능

--- view_models/task_manager_viewmodel.py
+++ view_models/task_manager_viewmodel.py
@@ -1,7 +1,53 @@
 # view_models/task_manager_viewmodel.py
 from PyQt6.QtCore import Qt, QObject, pyqtSignal, pyqtSlot
+from pathlib import Path
+from services.sequence_service import SequenceService
+from core.event_bus import EVENT_BUS
+
+
+
+"""
+- UI 의 상태 관리
+- UI 의 이벤트 처리
+- 명령 및 로직 요청
+"""
 
 
 
 class TaskManagerViewModel(QObject):
-    """"""
+
+    # View에게 상태를 알리는 시그널
+    sequence_data_loaded_complete = pyqtSignal(dict)     # 파일 읽기 성공
+    sequence_data_loaded_failed = pyqtSignal(str)        # 파일 읽기 실패
+
+
+    def __init__(self, sequence_service: SequenceService):
+        super().__init__()
+
+        # 로그 메세지의 말머리(로그 발생 위치 표시)
+        self._log_prefix = f"[{self.__class__.__name__}]"
+
+        self._sequence_service = sequence_service
+
+
+
+
+    def load_sequence_data(self, file_path: Path):
+        """시퀀스 파일의 데이터 읽어서 뷰에게 전달"""
+
+        EVENT_BUS.ui_log_message.emit(f"{self._log_prefix} 시퀀스 데이터 읽기 시작", "DEBUG")
+
+        # Service에게 시킴
+        sequence_data = self._sequence_service.load_sequence_file(file_path)
+
+        """
+        if sequence_data:
+            # 성공
+            self.sequence_data_loaded_complete.emit(sequence_data)
+            EVENT_BUS.ui_log_message.emit(f"{self._log_prefix} 시퀀스 데이터 읽기 성공", "INFO")
+
+        else:
+            # 실패
+            self.sequence_data_loaded_failed.emit(f"시퀀스 데이터 읽기 실패")
+            EVENT_BUS.ui_log_message.emit(f"{self._log_prefix} 시퀀스 데이터가 비어있거나 읽기 실패", "ERROR")
+        """
--- workers/sequence_worker.py
+++ workers/sequence_worker.py
@@ -1,10 +1,8 @@
 # view_models/sequence_worker.py
-"""
-ViewModel을 대신해서 Model의 작업을 대신 호출한다
-"""
+from pathlib import Path
 from PyQt6.QtCore import QObject, pyqtSignal
-from ..models.sequence_model import SequenceModel
-
+from models.sequence_model import SequenceModel
+from utils.file_handler import load_text, load_csv
 
 
 
@@ -16,25 +14,40 @@ class SequenceWorker(QObject):
     ViewModel은 나에게 일을 시키고 UI 스레드로 복귀 - 앱 멈춤 없다
     """
 
-    # ViewModel이 구독할 시그널(실제 아님. 예제 시그널)
-    completed = pyqtSignal(str)
-    failed = pyqtSignal(str)
+    # 로컬 시그널 (Service가 구독)
+    result = pyqtSignal(bool, str, list)    # 작업 결과 알림(성공여부, 메시지, 읽은 데이터)
+    finished = pyqtSignal()                 # 작업 종료 알림
 
 
-    def __init__(self):
+    def __init__(self, file_path: Path):
         super().__init__()
+
+        self._log_prefix = f"[{self.__class__.__name__}]"
+
+        self._file_path = file_path
 
         # Model 인스턴스
         self.model = SequenceModel()
 
 
-
     def run(self):
+        """시간이 오래 걸리는 비동기 작업(시퀀스 파일 읽기)을 수행"""
+
+        sequence_data = None
+
         try:
-            # 시간이 오래 걸리는 Model의 작업을 대신 호출
-            result = self.model.
-            # ViewModel에게 작업 완료 보고
-            self.completed.emit(result)
-            pass
+            sequence_data = load_csv(self._file_path)
+
+            print(f"{self._file_path}")
+
+            # 작업 결과 보고
+            # argument 순서: 성공여부, 메시지, 읽은 데이터
+            self.result.emit(True, f"{self._log_prefix} 파일({self._file_path}) 읽기 완료", sequence_data)
+
         except Exception as e:
-            pass
+            # 작업 결과 보고
+            self.result.emit(False, f"{self._log_prefix} 파일({self._file_path}) 읽기 실패: {e}", sequence_data)
+
+        finally:
+            # 작업 종료 알림
+            self.finished.emit()
