577db32680b26c5f8583f5d1275e415cee209d79
ChunHee Han <chunhee.han@outlook.com>
2025-12-09 13:01:11 +0900

    feat(viewmodel-task-manager): 시퀀스 데이터 시그널 처리
    - 시그널 구독
    - 지역 변수에 캐싱
    - 뷰의 START 버튼 클릭 이벤트 처리 연결

--- view_models/task_manager_viewmodel.py
+++ view_models/task_manager_viewmodel.py
@@ -1,16 +1,14 @@
 # view_models/task_manager_viewmodel.py
-from PyQt6.QtCore import Qt, QObject, pyqtSignal, pyqtSlot
-from pathlib import Path
-from services.sequence_service import SequenceService
-from core.event_bus import EVENT_BUS
-
-
-
 """
 - UI 의 상태 관리
 - UI 의 이벤트 처리
 - 명령 및 로직 요청
 """
+from pathlib import Path
+from PyQt6.QtCore import Qt, QObject, pyqtSignal, pyqtSlot
+from services.sequence_service import SequenceService
+from services.plc_service import PLCService
+from core.event_bus import EVENT_BUS
 
 
 
@@ -27,27 +25,39 @@ class TaskManagerViewModel(QObject):
         # 로그 메세지의 말머리(로그 발생 위치 표시)
         self._log_prefix = f"[{self.__class__.__name__}]"
 
+        # 서비스 객체 생성
         self._sequence_service = sequence_service
+        self._plc_service = PLCService()
+
+        # 시퀀스 데이터 시그널 데이터 저장 & 구독
+        self._cached_data = None
+        EVENT_BUS.sequence_data_updated.connect(self._on_sequence_data_updated)
 
 
+    
 
 
     def load_sequence_data(self, file_path: Path):
-        """시퀀스 파일의 데이터 읽어서 뷰에게 전달"""
+        """View의 LOAD 버튼 클릭 이벤트 처리"""
 
         EVENT_BUS.ui_log_message.emit(f"{self._log_prefix} 시퀀스 데이터 읽기 시작", "DEBUG")
 
         # Service에게 시킴
-        sequence_data = self._sequence_service.load_sequence_file(file_path)
+        self._sequence_service.load_sequence_file(file_path)
 
-        """
-        if sequence_data:
-            # 성공
-            self.sequence_data_loaded_complete.emit(sequence_data)
-            EVENT_BUS.ui_log_message.emit(f"{self._log_prefix} 시퀀스 데이터 읽기 성공", "INFO")
 
-        else:
-            # 실패
-            self.sequence_data_loaded_failed.emit(f"시퀀스 데이터 읽기 실패")
-            EVENT_BUS.ui_log_message.emit(f"{self._log_prefix} 시퀀스 데이터가 비어있거나 읽기 실패", "ERROR")
-        """
+    def start_sequence(self):
+        """View의 START 버튼 클릭 이벤트 처리"""
+        EVENT_BUS.ui_log_message.emit(f"{self._log_prefix} 시퀀스 실행 시작", "DEBUG")
+
+        # 방어코드 - 시퀀스 데이터가 없으면 서비스 호출 안 함
+        if not self._cached_data: return
+
+        EVENT_BUS.ui_log_message.emit(f"{self._log_prefix} 시퀀스 실행 요청 (데이터 {len(self._cached_data)}건)", "INFO")
+
+
+    # --- 슬롯 메서드 --- #
+    @pyqtSlot(dict)
+    def _on_sequence_data_updated(self, data: dict):
+        """Event Bus를 통해 온 시퀀스 데이터를 캐싱"""
+        self._cached_data = data
