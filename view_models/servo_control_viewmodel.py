742ede697b9905a4e0474d619085d8a6899b1fb1
ChunHee Han <chunhee.han@outlook.com>
2025-12-24 18:21:01 +0900

    feaet(viewmodel-servo-control): ServoControlWidget의 버튼 이벤트 처리 위임

--- view_models/servo_control_viewmodel.py
+++ view_models/servo_control_viewmodel.py
@@ -7,6 +7,46 @@ from communication.servo_adapter import ServoAdapter
 
 class ServoControlViewModel(QObject):
     """서보모터 제어용 뷰모델"""
-    def __init__(self, servo_adapter: ServoAdapter):
+    def __init__(self, plc_service: PLCService):
+        """
+        인자:
+            plc_service: 앱 전체에서 공유되는 PLC 서비스 인스턴스
+        """
         super().__init__()
-        self.servo = servo_adapter
+        self._log_prefix = f"[{self.__class__.__name__}]"
+        self._plc_service = plc_service
+
+    def start_manual(self, data: dict[str, float]):
+        """
+        수동 동작 시작
+        단위 변환 및 상세 제어 로직은 Adapter/Model 레이어로 위임
+        """
+        try:
+            EVENT_BUS.log.message.emit(f"{self._log_prefix} 서보모터 수동 동작 시작 요청: {data}", "INFO")
+            
+            # 서비스에 명령 위임
+            # PLCService.move_robot()은 내부적으로 QThread와 Worker를 생성, UI 멈춤 없이 비동기로 통신을 수행
+            self._plc_service.move_robot_by_pose(data)
+
+        except Exception as e:
+            EVENT_BUS.log.message.emit(f"{self._log_prefix} 서보모터 수동 동작 시작 실패: {e}", "ERROR")
+
+    def stop_manual(self):
+        """모든 축 정지"""
+        try:
+            EVENT_BUS.log.message.emit(f"{self._log_prefix} 서보모터 수동 동작 정지 명령", "INFO")
+            self._plc_service.emergency_stop_all()
+        except Exception as e:
+            EVENT_BUS.log.message.emit(f"{self._log_prefix} 서보모터 수동 동작 정지 실패: {e}", "ERROR")
+
+    def home_manual(self):
+        """모든 축(혹은 지정된 축) 원점 복귀"""
+        try:
+            EVENT_BUS.log.message.emit(f"{self._log_prefix} 서보모터 수동 원점 복귀 명령", "INFO")
+            # 턴테이블(Axis 3) 원점 복귀 우선 수행
+            self._plc_service.homing(3)
+            # 툴(1, 2)도 필요 시 추가
+            self._plc_service.homing(1)
+            self._plc_service.homing(2)
+        except Exception as e:
+            EVENT_BUS.log.message.emit(f"{self._log_prefix} 서보모터 수동 원점 복귀 실패: {e}", "ERROR")
