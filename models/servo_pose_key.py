ba2fbfbbc8f7ed040ddf6250587b59c53a04e3e3
ChunHee Han <chunhee.han@outlook.com>
2025-12-26 16:10:10 +0900

    feat(servo-pose-key): 에러 코드 추가

--- models/servo_pose_key.py
+++ models/servo_pose_key.py
@@ -61,11 +61,12 @@ class ServoSignal(str, Enum):
     TARGET_VEL   = 'MAIN.vel{}'         # 목표 속도 입력 (LREAL) - 위치/속도 모드 공용
 
     # [피드백 (Read)]
-    BUSY         = 'MAIN.Busy{}'        # 이동 중 여부 (BOOL)
+    BUSY         = 'MAIN.Busy{}'        # 이동 중 여부 (BOOL) - 명령 받아도 write ❌
     ACT_POS      = 'MAIN.Act_pos{}'     # 현재 위치 (LREAL)
     ACT_VEL      = 'MAIN.Act_vel{}'     # 현재 속도 (LREAL)
 
     # [에러]
+    ERROR_ID     = 'MAIN.nErrorID{}'    # 에러 코드 (UDINT/UINT)
     ERROR_RESET  = 'MAIN.bReset{}'      # 에러 리셋 신호 (BOOL)
     ERROR_STATE  = 'MAIN.bError{}'      # 에러 발생 상태 (BOOL)
 
