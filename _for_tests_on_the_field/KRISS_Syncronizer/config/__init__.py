6434f61f9b3eb718f7097480390d8cf61bb5dc18
ChunHee Han <chunhee.han@outlook.com>
2026-01-06 17:19:13 +0900

    Stop tracking _for_tests_on_the_field directory

--- _for_tests_on_the_field/KRISS_Syncronizer/config/__init__.py
+++
@@ -1,6 +0,0 @@
-# config/__init__.py
-from .paths import CONFIG_MACRO_PATH
-
-__all__ = [
-    'CONFIG_MACRO_PATH'
-]
--- _for_tests_on_the_field/KRISS_Syncronizer/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# utils/__init__.py
-from .env import AppEnv
-from .file_handler import save_json, load_json
-
-
-# from utils import * 로 임포트할 때 노출할 이름 목록
-__all__ = ["AppEnv", "save_json", "load_json"]
