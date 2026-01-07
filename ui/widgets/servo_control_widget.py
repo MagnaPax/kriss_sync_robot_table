879a1aa59a7ae36e382b6e86a03122ce7ab2ef5f
ChunHee Han <chunhee.han@outlook.com>
2026-01-07 14:49:59 +0900

    feat(widget-servo-control): VM의 시그널을 연결해서 화면 초기화

--- ui/widgets/servo_control_widget.py
+++ ui/widgets/servo_control_widget.py
@@ -59,6 +59,7 @@ class ServoControlWidget(BaseWidget):
 
         # 로봇과 턴테이블의 바쁨 상태 연결
         self.vm.busy_state_changed.connect(self.update_data)
+        self.vm.servo_inputs_clear.connect(self.clear_widget)
 
     def _bind_events(self):
         """UI 이벤트 바인딩"""
