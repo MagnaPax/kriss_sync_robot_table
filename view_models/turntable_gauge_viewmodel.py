68c12d2bd19f48c5c20f3b13ebbb5bb6b72a7913
MagnaPax <chunhee.han@outlook.com>
2026-01-12 09:25:34 +0900

    feat(vm-turntable-gauge): Create 파일

---
+++ view_models/turntable_gauge_viewmodel.py
@@ -0,0 +1,12 @@
+# view_models/turntable_gauge_viewmodel.py
+from PyQt6.QtCore import QObject
+
+
+class TurntableGaugeViewModel(QObject):
+    """"""
+    # TODO: 
+    # Waypoints 에 표시되는 데이터들 connect 시켜야 함
+    # 아래에서 다뤄지는 데이터들 받아서 connect 시켜야 된다
+    # TaskManagerViewModel
+    # TargetPositionViewModel
+    # ServoControllerViewModel
