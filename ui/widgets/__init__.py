d60bbb0aba70ab58ac0fb4a7f511cd2824d24079
ChunHee Han <chunhee.han@outlook.com>
2026-01-12 15:47:02 +0900

    fix: __init__.py 순환참조 오류 방지

--- ui/widgets/__init__.py
+++ ui/widgets/__init__.py
@@ -1,11 +1,2 @@
 # ui/widgets/__init__.py
-from .base_widget import BaseWidget
-from .logo_widget import LogoWidget
-from .turntable_gauge import TurntableWidget
-from .robot_position_widget import RobotPositionWidget
-from .current_state_widget import CurrentStateWidget
-
-
-
-
-__all__ = ['BaseWidget', 'LogoWidget', 'TurntableWidget', 'RobotPositionWidget', 'CurrentStateWidget']
+# (Empty to avoid circular imports and force explicit submodule imports)
