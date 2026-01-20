aaf44b40ead7ee3e54d81aea362abc12f5f7202b
MagnaPax <chunhee.han@outlook.com>
2026-01-20 19:48:45 +0900

    feat(event-bus): 로봇 이동 상태 시그널

--- core/event_bus.py
+++ core/event_bus.py
@@ -297,6 +297,9 @@ class ControlSignals(QObject):
     servo_physical_moving_status_changed = pyqtSignal(dict)
     """서보모터의 물리적인 이동 상태 변경 알림"""
 
+    robot_moving_status_changed = pyqtSignal(dict)
+    """로봇의 이동 상태 변경 알림"""
+
 # =============================================================================
 # 2. 실제 QObject
 # =============================================================================
