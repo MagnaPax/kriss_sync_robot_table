62a1058d61249bb3eadf541dc86003095e37e012
ChunHee Han <chunhee.han@outlook.com>
2026-02-10 15:02:23 +0900

    refactor(fanuc-adapter): 로봇 신호 초기화 & 시작

--- communication/fanuc_adapter.py
+++ communication/fanuc_adapter.py
@@ -72,19 +72,24 @@ class FanucAdapter:
         
         plc.write_by_name("MAIN.Robot1._UI1.UI_Byte3", new_ui3, pyads.PLCTYPE_BYTE)
 
+    def init_robot_signals(self):
+        """로봇을 움직일 수 있는 신호로 초기화"""
+        plc = self._plc
+        plc.write_by_name(FanucSignal.IMSP.path, False, pyads.PLCTYPE_BOOL)
+        plc.write_by_name(FanucSignal.HOLD.path, False, pyads.PLCTYPE_BOOL)
+        plc.write_by_name(FanucSignal.SFSP.path, False, pyads.PLCTYPE_BOOL)
+        plc.write_by_name(FanucSignal.FAULT_RESET.path, False, pyads.PLCTYPE_BOOL)
+        plc.write_by_name(FanucSignal.ENABLE.path, False, pyads.PLCTYPE_BOOL)
+
     def set_emergency_stop(self):
         """[비상 정지]"""
         plc = self._plc
         plc.write_by_name(FanucSignal.IMSP.path, False, pyads.PLCTYPE_BOOL)
 
-    def set_initial_signals(self):
-        """동작 가능하게 신호 초기화"""
+    def start_robot(self):
+        """로봇 시작 신호 전송"""
         plc = self._plc
-        plc.write_by_name(FanucSignal.IMSP.path, True, pyads.PLCTYPE_BOOL)
-        plc.write_by_name(FanucSignal.HOLD.path, True, pyads.PLCTYPE_BOOL)
-        plc.write_by_name(FanucSignal.SFSP.path, True, pyads.PLCTYPE_BOOL)
-        plc.write_by_name(FanucSignal.ENABLE.path, True, pyads.PLCTYPE_BOOL)
-        plc.write_by_name(FanucSignal.FAULT_RESET.path, True, pyads.PLCTYPE_BOOL)
+        plc.write_by_name(FanucSignal.START.path, True, pyads.PLCTYPE_BOOL)
 
     def stop_fanuc_normally(self):
         """로봇 정지 신호 전송"""
